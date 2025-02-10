298c0d8ecb92925c7c306a3671222a93e179bba1
USERNAME <EMAIL>
2025-02-10 15:19:47 -0500

    added rotate sensor for algaehandler + setjawangle command (Garbiel, Jelver)

--- src/main/java/frc/robot/commands/basic/AlgaeIn.java
+++ src/main/java/frc/robot/commands/basic/AlgaeIn.java
@@ -25,7 +25,7 @@ public class AlgaeIn extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    algaeHandler.in();
+    algaeHandler.eat();
   }
 
   // Called once the command ends or is interrupted.
--- src/main/java/frc/robot/commands/basic/AlgaeOut.java
+++ src/main/java/frc/robot/commands/basic/AlgaeOut.java
@@ -25,7 +25,7 @@ public class AlgaeOut extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    algaeHandler.out();
+    algaeHandler.spit();
   }
 
   // Called once the command ends or is interrupted.
