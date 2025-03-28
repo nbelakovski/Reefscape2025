8633d44b00616b7ee707133d2a152f64e7663a6c
gmoney1124 <gmoney12409@gmail.com>
2025-03-28 18:56:49 -0400

    made tag id closest id

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -118,10 +118,13 @@ public class Vision extends SubsystemBase {
     }
 
     // int tagId = tagChooser.getSelected();
-    int tagId = 21;
-    SmartDashboard.putNumber("tag " + tagId + " pose x", FieldConstants.aprilTagFieldLayout.getTagPose(tagId).get().getX());
-    SmartDashboard.putNumber("tag " + tagId + " pose y", FieldConstants.aprilTagFieldLayout.getTagPose(tagId).get().getY());
-    SmartDashboard.putNumber("tag " + tagId + " angle", FieldConstants.aprilTagFieldLayout.getTagPose(tagId).get().getRotation().getAngle());
+    int tagId = getClosestId();
+
+    if(tagId > 0) {
+      SmartDashboard.putNumber("tag " + tagId + " pose x", FieldConstants.aprilTagFieldLayout.getTagPose(tagId).get().getX());
+      SmartDashboard.putNumber("tag " + tagId + " pose y", FieldConstants.aprilTagFieldLayout.getTagPose(tagId).get().getY());
+      SmartDashboard.putNumber("tag " + tagId + " angle", FieldConstants.aprilTagFieldLayout.getTagPose(tagId).get().getRotation().getAngle());
+    }
 
     SmartDashboard.putNumber("closest Id", getClosestId());
     SmartDashboard.putNumber("closest x", getXDesired(getDesiredTarget(closestId)));
