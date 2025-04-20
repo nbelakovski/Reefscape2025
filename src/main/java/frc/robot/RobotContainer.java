bae76cc538370ea3bc5eeef9bc92f0892117ea5c
Nickolai Belakovski <nbelakovski@gmail.com>
2025-04-20 00:28:11 -0500

    Cleanup an unused command and move fieldCentric to an inline definition

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -16,6 +16,7 @@ import edu.wpi.first.wpilibj.XboxController.Button;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -71,7 +72,11 @@ public class RobotContainer {
     ));
 
     // Driver - X - Toggle FieldCentric on/off
-    new JoystickButton(driverController,Button.kX.value).toggleOnTrue(new ToggleFieldCentric());
+    InstantCommand toggleFieldCentric = new InstantCommand(() -> {
+      Drivetrain dt = Drivetrain.getInstance();
+      dt.setFieldCentric(!dt.fieldCentric);
+    }, Drivetrain.getInstance());
+    new JoystickButton(driverController,Button.kX.value).toggleOnTrue(toggleFieldCentric);
 
     // Driver - DPAD - Align to AprilTag Branch LEFT or RIGHT
     new Trigger(() -> driverController.getPOV() == 270).whileTrue(new DriveToClosestBranch("LEFT"));
