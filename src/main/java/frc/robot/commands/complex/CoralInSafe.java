d011e6b85498fbebd87cc1c980c2230e0956714c
Nickolai Belakovski <nbelakovski@gmail.com>
2025-04-25 19:07:17 -0400

    Refactor CoralInSafe since it seems we don't need to check for stop in run, or even to 'run' at all

--- src/main/java/frc/robot/commands/complex/CoralInSafe.java
+++ src/main/java/frc/robot/commands/complex/CoralInSafe.java
@@ -9,17 +9,13 @@ import frc.robot.subsystems.CoralScorer;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class CoralInSafe extends Command {
-
   private static CoralIntake coralIntake;
   private static CoralScorer coralScorer;
-  private boolean stop;
-
 
   /** Creates a new CoralIn. */
   public CoralInSafe() {
     coralIntake = CoralIntake.getInstance();
     coralScorer = CoralScorer.getInstance();
-    stop = false;
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(coralIntake, coralScorer);
   }
@@ -27,27 +23,13 @@ public class CoralInSafe extends Command {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    coralIntake.stop();
-    coralScorer.stop();
+    coralIntake.eat();
+    coralScorer.spitSlow();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {
-    
-    stop = coralIntake.isGapBlocked() && coralScorer.hasCoral();
-    
-    //If coral is completely inside of the scorer then were done intaking 
-    if(stop){
-      coralIntake.stop();
-      coralScorer.spitSlow(); //???
-    }
-    else{
-      // coralScorer.changeSpeed(MechConstants.INTAKE_SPEED);
-      coralIntake.eat();
-      coralScorer.spitSlow();
-    }
-  }
+  public void execute() {}
 
   // Called once the command ends or is interrupted.
   @Override
@@ -59,6 +41,7 @@ public class CoralInSafe extends Command {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return stop;
+    //If coral is completely inside of the scorer then were done intaking 
+    return coralIntake.isGapBlocked() && coralScorer.hasCoral();
   }
 }
